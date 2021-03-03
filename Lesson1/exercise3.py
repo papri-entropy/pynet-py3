07ce8bd0db41b58981d777c6309cb95d3d40312d
papri-entropy <cosminpetrache4@gmail.com>
2021-03-03 23:11:59 +0000

    exercise 1,2 and 3

---
+++ Lesson1/exercise3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+ 3.   Create three different variables the first variable should use all lower case characters with underscore ( _ ) as the word separator. 
+ The second variable should use all upper case characters with underscore as the word separator. 
+ The third variable should use numbers, letters, and underscore, but still be a valid variable Python variable name.
+
+Make all three variables be strings that refer to IPv6 addresses.
+
+Use the from future technique so that any string literals in Python2 are unicode.
+
+compare if variable1 equals variable2
+compare if variable1 is not equal to variable3
+"""
+
+from __future__ import unicode_literals
+
+first_variable = "2001::1:2:3:4"
+SECOND_VARIABLE = "2001::1:2:3:4"
+Variable_3 = "2001::1:2:3:4"
+
+print(first_variable == SECOND_VARIABLE)
+print(first_variable != Variable_3)
+
+print()
+print("Is variable1 equal to variable2: {}".format(first_variable == SECOND_VARIABLE))
+print("Is variable1 not equal to variable3: {}".format(first_variable != Variable_3))
+print()
