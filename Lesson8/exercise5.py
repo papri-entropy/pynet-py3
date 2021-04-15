9b774087411a7b863a0b056b9e64b6e4b86d3aba
papri-entropy <cosminpetrache4@gmail.com>
2021-04-15 15:32:22 +0100

    lesson8 exercises

---
+++ Lesson8/exercise5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+Write a python program where you store a device in an external Python module as a dictionary.
+Pass that device into Netmiko and connect to the device successfully and print out the device's
+prompt.
+Repeat this, but with three device's
+"""
+
+this exercise was done on linux test box with connectivity to arista
